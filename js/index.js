fc74a9bc21a0bba9b9857295ad2166b42311cd54
jlugod1 <jlugopachecom@gmail.com>
2024-09-05 22:05:41 -0400

    me gusta core

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var botondefinicion =document.getElementById("boton1");
+
+botondefinicion.addEventListener("click", function() {
+   
+    var megusta1 =document.getElementById("megusta1");
+    megusta1.textContent = Number(megusta1.textContent) +1;
+  
+
+
+})
+
+var megusta2 =document.getElementById("boton2");
+
+megusta2.addEventListener("click", function() {
+   
+   
+    var megusta2 =document.getElementById("megusta2");
+    megusta2.textContent = Number(megusta2.textContent) +1  })
+
+   
+   
+    var megusta3 =document.getElementById("boton3");
+
+megusta3.addEventListener("click", function() {
+    
+    var megusta3 =document.getElementById("megusta3");
+    megusta3.textContent = Number(megusta3.textContent) +1;})
